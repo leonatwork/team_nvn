feba8cda1287574c2ae7f1e6377f970c302ee7c6
Nitish Rana <nitishrana1998@gmail.com>
2018-02-08 22:01:58 +0530

    Fixed score bug

--- script2.py
+++ script2.py
@@ -95,6 +95,10 @@ for i in range(5):
                 print("\033[H\033[J")
             k = k+1
             time.sleep(2)
+file = open("score.txt","w")
+file.write(str(scUser))
+file.write(str(scComp))
+file.close()
 k=0
 for i in range(5):
     print("\n"+user_move[k]+" Vs "+comp_move[k])
